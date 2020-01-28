d67fa5a90ca9f2ae7ef60cada39143ae4d1b47bd
Derek Wright <derek.wright@colorado.edu>
2020-01-28 16:51:07 -0700

    add lab2

---
+++ Lab2/Wright_RTOS_Lab2_GPIO_Timers_Interrupts/src/Header_Files/gpio.h
@@ -0,0 +1,48 @@
+//***********************************************************************************
+// Include files
+//***********************************************************************************
+#include "em_gpio.h"
+#include "bspconfig.h"
+
+//***********************************************************************************
+// defined files
+//***********************************************************************************
+
+// LED 0 pin is
+#define	LED0_port		gpioPortF
+#define LED0_pin		4
+#define LED0_default	false 	// Default false (0) = off, true (1) = on
+// LED 1 pin is
+#define LED1_port		gpioPortF
+#define LED1_pin		5
+#define LED1_default	false	// Default false (0) = off, true (1) = on
+
+//***********************************************************************************
+// global variables
+//***********************************************************************************
+
+
+//***********************************************************************************
+// function prototypes
+//***********************************************************************************
+void gpio_open(void);
+
+
+
+/**
+ * sample_button()
+ * ___________________
+ * @description This function returns the state of a pushbutton.
+ * 				Two macro functions are provided to specifically
+ * 				sample pushbutton 0 and 1.
+ *
+ * @parameters
+ * 	- uint8_t button_pin: the pin number we want the state of.
+ * 							In our case, either 6 or 7.
+ * @return
+ *		- uint8_t: 1 if button is UNpressed, 0 if button is PRESSED.
+ */
+uint8_t sample_button(uint8_t button_pin);
+#define sample_button0(...)		sample_button(BSP_GPIO_PB0_PIN)
+#define sample_button1(...)		sample_button(BSP_GPIO_PB1_PIN)
+

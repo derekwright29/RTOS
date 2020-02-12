c931dbc6c1ac38674ee1989da9d0bd6b9acea783
Derek Wright <derek.wright@colorado.edu>
2020-02-12 12:47:45 -0700

    destroy comments in fcfs.c

--- Lab4/PartA/fcfs.c
+++ Lab4/PartA/fcfs.c
@@ -7,6 +7,7 @@ void init(struct task_t *task, int *execution, int size) {
     for (int i = 0; i < size; i++)
     {
         task[i].process_id = i;
+        // ternary operator catches negative execution values, and sets them to 0
         task[i].execution_time = execution[i] < 0 ? 0: execution[i];
     }
 }
@@ -16,27 +17,20 @@ void init(struct task_t *task, int *execution, int size) {
  * A queue is initialized in this function, and at the end the queue
  * is empty, meaning there are no tasks in the queue, and all pointers
  * malloc during its initialization have been freed.
- * 
 */
 void first_come_first_served(struct task_t *task, int size) {
     int time_elapsed = 0;
     // Create Queue based on the task array
     struct node_t* head = create_queue(task, size);
     // Pop off the front item until there are no more tasks
-    printf("\nIn FCFS \n" );
-    int i = 0;
     while (!is_empty(&head)) 
     {
-        printf("\nIn FCFS loop %d\n", i++ );
         //get current task
         struct task_t *cur_task = peek(&head);
         //update stat info on current task based on time elapsed and its execution time
         cur_task->turnaround_time = time_elapsed +  cur_task->execution_time;
         cur_task->waiting_time = time_elapsed;
-        printf("The turnaround is %d\n", cur_task->turnaround_time);
-        printf("The waiting is %d\n", cur_task->waiting_time);
         //update time_elapsed. Simulates the task running to completion
-        printf("The time elapsed is %d\n", time_elapsed);
         time_elapsed += cur_task->execution_time;
         //remove cur_task from the queue.
         pop(&head); 
@@ -45,6 +39,7 @@ void first_come_first_served(struct task_t *task, int size) {
     empty_queue(&head);
 }
 
+// a simple average with waiting_time variable
 float calculate_average_wait_time(struct task_t *task, int size) {
     float sum = 0.0;
     for (int i = 0; i< size; i++) {
@@ -54,11 +49,11 @@ float calculate_average_wait_time(struct task_t *task, int size) {
     return sum / size;
 }
 
+// copy/paste from above, but with the different member variable, turnaround_time instead.
 float calculate_average_turn_around_time(struct task_t *task, int size) {
     float sum = 0.0;
     for (int i = 0; i< size; i++) {
         sum += (float) task[i].turnaround_time;
-    }
-    
+    }  
     return sum / size;
 }

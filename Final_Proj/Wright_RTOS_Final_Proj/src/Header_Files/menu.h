9d62690c6d47677c98244d5f81554f4b9a16eae3
Derek Wright <derek.wright@colorado.edu>
2020-04-19 23:06:42 -0600

    Start menu works

--- Final_Proj/Wright_RTOS_Final_Proj/src/Header_Files/menu.h
+++ Final_Proj/Wright_RTOS_Final_Proj/src/Header_Files/menu.h
@@ -28,11 +28,11 @@ OS_SEM game_over_sem;
 static GLIB_Context_t menu_context;
 
 
-const vehicle_description_t clunker = VEHICLE_DESC_STRUCT_CLUNKER;
-const vehicle_description_t sport = VEHICLE_DESC_STRUCT_SPORT;
-const vehicle_description_t truck = VEHICLE_DESC_STRUCT_TRUCK;
-
-const vehicle_description_t car_choices[] = {   clunker, sport, truck   };
+//vehicle_description_t clunker = VEHICLE_DESC_STRUCT_CLUNKER;
+//vehicle_description_t sport = VEHICLE_DESC_STRUCT_SPORT;
+//vehicle_description_t truck = VEHICLE_DESC_STRUCT_TRUCK;
+//
+//vehicle_description_t * car_choices[3];
 
 
 
@@ -43,9 +43,9 @@ enum screentypes {
 	CHOOSE_COURSE,
 	CHOOSE_COND,
 	READY_TO_START,
+	GAME_OVER,
+	STATS
 };
-
-
 
 
 void toggle_menu_control(void);
@@ -59,5 +59,9 @@ void menu_init(void);
 void create_game_over_semaphore(void);
 
 void write_menu(uint8_t screen, uint8_t selection, bool select_active);
+
+void write_gameover(uint8_t screen);
+
+void redraw_selection(uint8_t screen, uint8_t selection) ;
 
 #endif /* SRC_HEADER_FILES_MENU_H_ */
